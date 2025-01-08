b2f5c12568f0c5d9f8568350aee2e961bb802959
zhangdanq <danqingzhang5@gmail.com>
2025-01-08 17:37:11 +0800

    modify 15 16 22 26 30

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -1,3 +1,53 @@
+// #include "../exercise.h"
+
+// // READ: 复制构造函数 <https://zh.cppreference.com/w/cpp/language/copy_constructor>
+// // READ: 函数定义（显式弃置）<https://zh.cppreference.com/w/cpp/language/function>
+
+
+// class DynFibonacci {
+//     size_t *cache;
+//     int cached;
+
+// public:
+//     // TODO: 实现动态设置容量的构造器
+//     DynFibonacci(int capacity): cache(new size_t[capacity]{0,1}), cached(2) {}
+
+//     // TODO: 实现复制构造器
+//     DynFibonacci(DynFibonacci const &others) :cache(new size_t[others.cached]), cached(others.cached) {
+//         std::memcpy(cache, others.cache, cached*sizeof(size_t)); //上面的是复制形状，这一行是复制内容
+//     };
+
+//     // TODO: 实现析构器，释放缓存空间
+//     ~DynFibonacci(){delete[] cache;};
+
+//     // TODO: 实现正确的缓存优化斐波那契计算
+//     size_t get(int i) {
+//         for (; cached <= i; ++cached) {
+//             cache[cached] = cache[cached - 1] + cache[cached - 2];
+//         }
+//         return cache[i];
+//     }
+
+//     // NOTICE: 不要修改这个方法
+//     // NOTICE: 名字相同参数也相同，但 const 修饰不同的方法是一对重载方法，可以同时存在
+//     //         本质上，方法是隐藏了 this 参数的函数
+//     //         const 修饰作用在 this 上，因此它们实际上参数不同
+//     size_t get(int i) const {
+//         if (i <= cached) {
+//             return cache[i];
+//         }
+//         ASSERT(false, "i out of range");
+//     }
+// };
+
+// int main(int argc, char **argv) {
+//     DynFibonacci fib(12);
+//     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+//     DynFibonacci const fib_ = fib;
+//     ASSERT(fib_.get(10) == fib.get(10), "Object cloned");
+//     return 0;
+// }
+
 #include "../exercise.h"
 
 // READ: 复制构造函数 <https://zh.cppreference.com/w/cpp/language/copy_constructor>
@@ -10,15 +60,24 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new size_t[capacity]{0,1}), cached(2) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &others) :cache(new size_t[others.cached]), cached(others.cached) {
-        std::memcpy(cache, others.cache, cached*sizeof(size_t)); //上面的是复制形状，这一行是复制内容
+    DynFibonacci(DynFibonacci const & d) {
+        cache = new size_t[d.cached];
+        for (int i = 0; i < d.cached; ++i) {
+            cache[i] = d.cache[i];
+        }
+        cached = d.cached;
     };
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci(){delete[] cache;};
+    ~DynFibonacci() {
+        delete [] cache;
+    };
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,4 +1,132 @@
-﻿#include "../exercise.h"
+﻿// #include "../exercise.h"
+
+// // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
+
+// template<class T>
+// struct Tensor4D {
+//     unsigned int shape[4];
+//     T *data;
+
+//     Tensor4D(unsigned int const shape_[4], T const *data_) {
+//         unsigned int size = 1;
+//         // TODO: 填入正确的 shape 并计算 size
+//         std::memcpy(shape, shape_, 4*sizeof(unsigned int));
+//         size = shape[0] * shape[1] * shape[2] * shape[3];
+//         data = new T[size];
+//         std::memcpy(data, data_, size * sizeof(T));
+//     }
+//     ~Tensor4D() {
+//         delete[] data;
+//     }
+
+//     // 为了保持简单，禁止复制和移动
+//     Tensor4D(Tensor4D const &) = delete;
+//     Tensor4D(Tensor4D &&) noexcept = delete;
+
+//     // 这个加法需要支持“单向广播”。
+//     // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
+//     // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
+//     // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
+//     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
+//     Tensor4D &operator+=(Tensor4D const &others) {
+//         // TODO: 实现单向广播的加法
+//         for(int i = 0;i<shape[0];i++){
+//             int ei = shape[0] != others.shape[0] ? 0: i;
+//             for(int j = 0;j<shape[1];j++){
+//                 int ej = shape[1] != others.shape[1] ? 0: j;
+//                 for(int k = 0;k<shape[2];k++){
+//                     int ek = shape[2] != others.shape[2] ? 0: k;
+//                     for(int l = 0;l<shape[3];l++){
+//                         int el = shape[3] != others.shape[3] ? 0: l;
+//                         int index = i * shape[1] * shape[2] * shape[3] + j * shape[2] * shape[3] + k * shape[3] + l;
+//                         int eindex = ei * others.shape[1] * others.shape[2] * others.shape[3] + ej * others.shape[2] * others.shape[3] + ek * others.shape[3] + el;
+//                         data[index] += others.data[eindex];
+//                     }
+//                 }
+//             }
+//         }
+//         return *this;
+//     }
+// };
+
+// // ---- 不要修改以下代码 ----
+// int main(int argc, char **argv) {
+//     {
+//         unsigned int shape[]{1, 2, 3, 4};
+//         // clang-format off
+//         int data[]{
+//              1,  2,  3,  4,
+//              5,  6,  7,  8,
+//              9, 10, 11, 12,
+
+//             13, 14, 15, 16,
+//             17, 18, 19, 20,
+//             21, 22, 23, 24};
+//         // clang-format on
+//         auto t0 = Tensor4D(shape, data);
+//         auto t1 = Tensor4D(shape, data);
+//         t0 += t1;
+//         for (auto i = 0u; i < sizeof(data) / sizeof(*data); ++i) {
+//             ASSERT(t0.data[i] == data[i] * 2, "Tensor doubled by plus its self.");
+//         }
+//     }
+//     {
+//         unsigned int s0[]{1, 2, 3, 4};
+//         // clang-format off
+//         float d0[]{
+//             1, 1, 1, 1,
+//             2, 2, 2, 2,
+//             3, 3, 3, 3,
+
+//             4, 4, 4, 4,
+//             5, 5, 5, 5,
+//             6, 6, 6, 6};
+//         // clang-format on
+//         unsigned int s1[]{1, 2, 3, 1};
+//         // clang-format off
+//         float d1[]{
+//             6,
+//             5,
+//             4,
+
+//             3,
+//             2,
+//             1};
+//         // clang-format on
+
+//         auto t0 = Tensor4D(s0, d0);
+//         auto t1 = Tensor4D(s1, d1);
+//         t0 += t1;
+//         for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
+//             ASSERT(t0.data[i] == 7.f, "Every element of t0 should be 7 after adding t1 to it.");
+//         }
+//     }
+//     {
+//         unsigned int s0[]{1, 2, 3, 4};
+//         // clang-format off
+//         double d0[]{
+//              1,  2,  3,  4,
+//              5,  6,  7,  8,
+//              9, 10, 11, 12,
+
+//             13, 14, 15, 16,
+//             17, 18, 19, 20,
+//             21, 22, 23, 24};
+//         // clang-format on
+//         unsigned int s1[]{1, 1, 1, 1};
+//         double d1[]{1};
+
+//         auto t0 = Tensor4D(s0, d0);
+//         auto t1 = Tensor4D(s1, d1);
+//         t0 += t1;
+//         for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
+//             ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
+//         }
+//     }
+// }
+
+#include "../exercise.h"
+#include <cstring>
 
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
@@ -10,8 +138,9 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
-        std::memcpy(shape, shape_, 4*sizeof(unsigned int));
+        std::memcpy(shape, shape_, 4 * sizeof(unsigned int));
         size = shape[0] * shape[1] * shape[2] * shape[3];
+
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -30,17 +159,34 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
-        for(int i = 0;i<shape[0];i++){
-            int ei = shape[0] != others.shape[0] ? 0: i;
-            for(int j = 0;j<shape[1];j++){
-                int ej = shape[1] != others.shape[1] ? 0: j;
-                for(int k = 0;k<shape[2];k++){
-                    int ek = shape[2] != others.shape[2] ? 0: k;
-                    for(int l = 0;l<shape[3];l++){
-                        int el = shape[3] != others.shape[3] ? 0: l;
-                        int index = i * shape[1] * shape[2] * shape[3] + j * shape[2] * shape[3] + k * shape[3] + l;
-                        int eindex = ei * others.shape[1] * others.shape[2] * others.shape[3] + ej * others.shape[2] * others.shape[3] + ek * others.shape[3] + el;
-                        data[index] += others.data[eindex];
+        int oi, oj, ok, ol;
+        for(int i = 0; i < shape[0]; i++){
+            if(others.shape[0] != shape[0]){
+                oi = 0;
+            }else{
+                oi = i;
+            }
+            for(int j = 0; j < shape[1]; j++){
+                if(others.shape[1] != shape[1]){
+                    oj = 0;
+                }else{
+                    oj = j;
+                }
+                for(int k = 0; k < shape[2]; k++){
+                    if(others.shape[2] != shape[2]){
+                        ok = 0;
+                    }else{
+                        ok = k;
+                    }
+                    for(int l = 0; l < shape[3]; l++){
+                        if(others.shape[3] != shape[3]){
+                            ol = 0;
+                        }else{
+                            ol = l;
+                        }
+                        auto index = i * shape[1] * shape[2] * shape[3] + j * shape[2] * shape[3] + k * shape[3] + l;
+                        auto oindex = oi * others.shape[1] * others.shape[2] * others.shape[3] + oj * others.shape[2] * others.shape[3] + ok * others.shape[3] + ol;
+                        data[index] += others.data[oindex];
                     }
                 }
             }
